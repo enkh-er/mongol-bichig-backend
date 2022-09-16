ce3f83514a052f32266115ecd210125f4b8782be
EnkhErdene0421 <enkhgem@gmail.com>
2022-09-16 20:36:26 +0800

    adf

--- src/main/java/stud/enkherdene/mongolbichig/MongolbichigApplication.java
+++ src/main/java/stud/enkherdene/mongolbichig/MongolbichigApplication.java
@@ -1,7 +1,14 @@
 package stud.enkherdene.mongolbichig;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @SpringBootApplication
 public class MongolbichigApplication {
